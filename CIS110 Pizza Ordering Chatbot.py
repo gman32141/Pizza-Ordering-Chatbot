cc266bc70a180fac63d1609c28345a0c85c08fab
username <useremail>
2025-04-27 18:59:29 -0700

    Added inputs and outputs`

--- CIS110 Pizza Ordering Chatbot.py
+++ CIS110 Pizza Ordering Chatbot.py
@@ -1,2 +1,19 @@
-print("Hello World")
+print("Hello my name is Alex your virtual assistant. I will help you` order pizza!")
+print("Please type your respense and press Enter.")
+userName = input("\nEnter your name:  ")
+print(f"\nHello, {userName}. Nice to meet you!")
+size = input("What size do you want? Enter small, medium, or large:  ")
+flavor = input("\nEnter the flavor of pizza:  ")
+crustType= input("\n`What type of crust do you want:  ")
+quantity = input("\nHow many of these do you want to order? Enter` a numeric value:  ")
+quantity = int(quantity)
+method = input("\nIs this carry out or delivery:  ")
+salesTax = 1.1
+pizzaCost = 14.99
+total = (pizzaCost * quantity) * salesTax
+print(total)
+print("-" * 10)
+print(f"Thank you, {userName}, for your order.")
+print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
+print("-" * 10)
 
