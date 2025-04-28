5a4b9abf8337fd7bd9c353666ed70ea4e86040a5
username <useremail>
2025-04-28 06:07:49 -0700

    Added` If-Else statements

--- CIS110 Pizza Ordering Chatbot.py
+++ CIS110 Pizza Ordering Chatbot.py
@@ -1,19 +1,36 @@
 print("Hello my name is Alex your virtual assistant. I will help you` order pizza!")
 print("Please type your respense and press Enter.")
 userName = input("\nEnter your name:  ")
+if userName. lower() == "gianni filippone":
+    print(f"\nMy creator, {userName}. Pleasure to serve you!")
+else:
+    print(f"\nHello, {userName}. Nice to meet you!")
 print(f"\nHello, {userName}. Nice to meet you!")
 size = input("What size do you want? Enter small, medium, or large:  ")
 flavor = input("\nEnter the flavor of pizza:  ")
-crustType= input("\n`What type of crust do you want:  ")
+crustType= input("\nWhat type of crust do you want:  ")
 quantity = input("\nHow many of these do you want to order? Enter` a numeric value:  ")
 quantity = int(quantity)
 method = input("\nIs this carry out or delivery:  ")
+if method.lower() == "delivery":
+    deliveryFee = 5
+else:
+    deliveryFee = 0
 salesTax = 1.1
-pizzaCost = 14.99
-total = (pizzaCost * quantity) * salesTax
+if size.lower() == "small":
+   pizzaCost = 8.99
+elif size.lower() == "medium":
+   pizzaCost = 14.99
+elif size.lower() == "large":
+   pizzaCost = 17.99
+total = (pizzaCost * quantity) * salesTax + deliveryFee
 print(total)
 print("-" * 10)
 print(f"Thank you, {userName}, for your order.")
 print(f"Your {quantity} {size} {flavor} pizza(s) with {crustType} crust costs ${total:,.2f}.")
+if total >= 50:
+    print("\nCongratulations! You've been awarded a $10 off coupon for your next order.")
+else:
+    print("\nOrder over $50 will receive a free $10 off coupon!")
 print("-" * 10)
-
+ 
